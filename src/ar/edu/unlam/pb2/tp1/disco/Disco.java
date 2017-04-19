6f688aa2edf9cf59a1316419f2179e610544fc64
Federico Bevacqua <fede.el.xeneize@gmail.com>
2017-04-18 21:50:59 -0300

    Agregados atributos y métodos correspondiente a la clase Disco.

--- src/ar/edu/unlam/pb2/tp1/disco/Disco.java
+++ src/ar/edu/unlam/pb2/tp1/disco/Disco.java
@@ -1,5 +1,42 @@
 package ar.edu.unlam.pb2.tp1.disco;
 
 public class Disco {
+	private Double radioInterior;
+	private Double radioExterior;
+	
+	public Disco(Double radioInterior, Double radioExterior) {
+		super();
+		this.setRadioInterior(radioInterior);
+		this.setRadioExterior(radioExterior);
+	}
+
+	public Double getRadioInterior() {
+		return radioInterior;
+	}
+	public void setRadioInterior(Double radioInterior) {
+		this.radioInterior = radioInterior;
+	}
+	public Double getRadioExterior() {
+		return radioExterior;
+	}
+	public void setRadioExterior(Double radioExterior) {
+		this.radioExterior = radioExterior;
+	}
+	
+	public Double obtenerPerimetroInterior() {
+		Double perimetroInterior = 2 * Math.PI * this.getRadioInterior();
+		return perimetroInterior;
+	}
+	
+	public Double obtenerPerimetroExterior() {
+		Double perimetroExterior = 2 * Math.PI * this.getRadioExterior();
+		return perimetroExterior;
+	}
+	
+	public Double obtenerSuperficie() {
+		Double superficieInterior = Math.PI * Math.pow(this.getRadioInterior(), 2);
+		Double superficieExterior = Math.PI * Math.pow(this.getRadioExterior(), 2);
+		return (superficieExterior - superficieInterior);
+	}
 
 }
